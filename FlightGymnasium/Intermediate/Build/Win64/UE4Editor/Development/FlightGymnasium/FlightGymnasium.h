f0e73af29441255e2ab5b7232473367a86fdee5e
unknown <Tech@C401IT09.SCT.local>
2016-01-27 11:17:51 -0500

    Initial commit, but for real
    FOR REALS

---
+++ FlightGymnasium/Intermediate/Build/Win64/UE4Editor/Development/FlightGymnasium/FlightGymnasium.h
@@ -0,0 +1,5 @@
+#ifndef __AUTO_FLIGHTGYMNASIUM_H__
+#define __AUTO_FLIGHTGYMNASIUM_H__
+//Last Write: 27/01/2016 4:11:24 PM +00:00
+#include "C:\Users\C401ITxx\Documents\Unreal Projects\FlightGymnasium\Source\FlightGymnasium\FlightGymnasium.h"
+#endif//__AUTO_FLIGHTGYMNASIUM_H__
