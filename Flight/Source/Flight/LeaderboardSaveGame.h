9fb96c26849b367e38e865815ddfbecb689c683a
Brian Bailey <brianbailey93@gmail.com>
2016-04-03 20:46:38 -0400

    Changed hud a little

---
+++ Flight/Source/Flight/LeaderboardSaveGame.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/SaveGame.h"
+#include "LeaderboardSaveGame.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FLIGHT_API ULeaderboardSaveGame : public USaveGame
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
